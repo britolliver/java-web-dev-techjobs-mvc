bd649e582ec4d3080ce135976ee0dc95153a79f6
Bri <bri39annat@gmail.com>
2021-06-21 23:10:00 -0500

    first commit

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/JavaWebDevTechjobsMvcApplication.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/JavaWebDevTechjobsMvcApplication.java
@@ -1,5 +1,6 @@
 package org.launchcode.javawebdevtechjobsmvc;
 
+import org.launchcode.javawebdevtechjobsmvc.controllers.ListController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
