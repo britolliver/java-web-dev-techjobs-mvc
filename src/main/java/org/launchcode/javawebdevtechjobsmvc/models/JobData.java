f053af26266247e6bedec33c0be1890bac58497f
Bri <bri39annat@gmail.com>
2021-06-22 23:41:45 -0500

    final commit, finished

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/models/JobData.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/models/JobData.java
@@ -19,7 +19,7 @@ public class JobData {
     private static final String DATA_FILE = "job_data.csv";
     private static boolean isDataLoaded = false;
 
-    private static ArrayList<Job> allJobs;
+    private static ArrayList<Job> allJobs = new ArrayList<>();
     private static ArrayList<Employer> allEmployers = new ArrayList<>();
     private static ArrayList<Location> allLocations = new ArrayList<>();
     private static ArrayList<PositionType> allPositionTypes = new ArrayList<>();
@@ -201,6 +201,12 @@ public class JobData {
             System.out.println("Failed to load job data");
             e.printStackTrace();
         }
+    }
+
+    public static ArrayList<Job> getAllJobs() {
+        loadData();
+        allJobs.sort(new NameSorter());
+        return allJobs;
     }
 
     public static ArrayList<Employer> getAllEmployers() {
